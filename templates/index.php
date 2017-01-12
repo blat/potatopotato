9440d1b0bb888f8daff5832669d9e762b44cfb51
blat <blatiere@gmail.com>
2017-01-12 09:12:36 +0100

    fix notices

--- templates/index.php
+++ templates/index.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="/style.css">
         <link href="/feed.xml" rel="alternate" type="application/rss+xml">
-        <link href="<?= $favicon ?: '/favicon.png' ?>" rel="icon">
+        <link href="<?= !empty($favicon) ? $favicon : '/favicon.png' ?>" rel="icon">
     </head>
     <body>
         <?php $this->insert('custom::header') ?>
